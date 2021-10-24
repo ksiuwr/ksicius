7f5554d1cfccfc9fb0ed756fb0c7da6ed3fd80c7
jjzajac <jakub.zajac.jan@gmail.com>
2021-10-24 12:25:19 +0200

    init

--- bot/.eslintrc.js
+++ bot/.eslintrc.js
@@ -1,19 +1,20 @@
 module.exports = {
-    "env": {
-        "es2021": true,
-        "node": true
-    },
-    "extends": [
-        "airbnb-base"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
-};
+  env: {
+    es6: true,
+    node: true,
+  },
+  extends: 'airbnb-base',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    project: 'tsconfig.json',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  rules: {
+    semi: ['error', 'never'],
+    quotes: [2, 'single'],
+  },
+}
